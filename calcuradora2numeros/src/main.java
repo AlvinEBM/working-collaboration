38f21b18abe0eac6d1a76f66b7be7924c1eb9b6a
ryamir <alvin_99_02@hotmail.com>
2025-05-04 18:42:26 -0500

    1er ejer

---
+++ calcuradora2numeros/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+    
+    public static void main(String[] args) {
+        calculadora cal=new calculadora();
+        System.out.println("suma de los enteros : "+cal.sumar(8, 10));
+        System.out.println("suma de 3 enteros : "+cal.sumar(10, 18, 19));
+        System.out.println("suma de 2 numeros decimales "+cal.sumar(15.2, 0.555));
+    
+    }
+    
+}
